c4d7dbb01c7516be25f920ab390f623f0b59292d
miguelr95 <miguelroldanmachado@gmail.com>
2016-04-17 13:34:39 +0200

    cambios perfil(falta unir con usuario padre)

--- angular-2/main/src/app/profile/profile.component.ts
+++ angular-2/main/src/app/profile/profile.component.ts
@@ -1,25 +1,28 @@
-import {Component} from 'angular2/core';
+import {Component,Input} from 'angular2/core';
 import{user} from './user.model';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 @Component({
   selector:'profile',
-  templateUrl: 'app/main/profile.component.html',
+  templateUrl: 'app/profile/profile.component.html',
   directives:[ROUTER_DIRECTIVES]
 })
 export class ProfileComponent{
-  
-  private usuarioNuevo:user;
+  @Input()
+  private usuario:user;
 
-  private viejaContraseña:string;
-  private nuevaContraseña:string;
-  private nuevaContraseñaC:string;
+
+  private nusuario =new user('../../img/logo.png','miguel','ahahahah@mdamdasds.com','1234','1995-02-05','miguelr95','roldan','pipipiip','popopp');
+  private usuarion=this.nusuario;
+  private viejaPass:string;
+  private nuevaPass:string;
+  private nuevaPassc:string;
   cambiarFoto(){
 
   }
-  cambiarDatos(){
-    if(this.viejaContraseña===this.usuario.contraseña){
-      if(this.nuevaContraseña===this.nuevaContraseñaC){
-        this.usuario=this.usuarioNuevo;
+  cambiarDatos(newUser:user){
+    if(this.viejaPass===this.usuario.pass){
+      if(this.nuevaPass===this.nuevaPassc){
+        this.nusuario=this.usuarion;
       }else{
         //mensaje de error
       }
--- angular-2/main/src/app/profile/user.model.ts
+++ angular-2/main/src/app/profile/user.model.ts
@@ -1,11 +1,29 @@
-export interface user{
-    fotoPrefil: URL;
+export class user{
+    fotoPerfil:string;
     nombre: string;
     correo:string;
-    contraseña:string;
+    pass:string;
     fechaNacimiento:string;
     steam: string;
     bnet:string;
     descripcion:string;
     descripcionMentor:string;
+
+    constructor(fotoPrefilc:string, nombrec: string, correoc:string, passc:string, fechaNacimientoc:string,
+    steamc: string,
+    bnetc:string,
+    descripcionc:string,
+    descripcionMentorc:string){
+      this.fotoPerfil=fotoPrefilc;
+      this.nombre=nombrec;
+      this.correo=correoc;
+      this.pass=passc;
+      this.fechaNacimiento=fechaNacimientoc;
+      this.steam=steamc;
+      this.bnet=bnetc;
+      this.descripcion=descripcionc;
+      this.descripcionMentor=descripcionMentorc;
+      console.log(this.fotoPerfil);
+    }
+
 }
