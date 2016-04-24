f4931ab1d17cfc7907b56f4b7b43a43aafcba887
valli22 <davidvacasmiguel@hotmail.com>
2016-04-24 17:21:51 +0200

    se colocan bien las oferta

--- angular-2/main/src/app/ofertas/ofertas.component.ts
+++ angular-2/main/src/app/ofertas/ofertas.component.ts
@@ -14,12 +14,39 @@ import {OfertasService} from '../service/ofertas.service.ts';
 
 export class OfertasComponent{
 
-  private ofertas : Oferta[];
+  private ofertas : Oferta[][] = [];
 
   constructor(private ofertasService : OfertasService){}
 
   ngOnInit(){
-    this.ofertas = this.ofertasService.getOfertas();
+     var ofertasE = this.ofertasService.getOfertas();
+     var i = 0;
+     var j = 0;
+     var ofertillas = [];
+     for(var oferta of ofertasE){
+       ofertillas.push(oferta);
+       j++;
+       if(j==2){
+         this.ofertas.push(ofertillas);
+         ofertillas=[];
+         i++;
+         j=0;
+       }
+     }
+     /*for (let i = 0; i < (ofertasE.length/2); i++) {
+         for (let j = 0; i < 2; i++){
+           console.log(this.ofertas[i]);
+           if(ofertasE.length>i*j){
+             if(i==0){
+               this.ofertas[i].push(ofertasE[j]);
+             }else{
+               this.ofertas[i].push(ofertasE[j*i]);
+             }
+           }else{
+             break;
+           }
+         }
+     }*/
   }
 
 }
