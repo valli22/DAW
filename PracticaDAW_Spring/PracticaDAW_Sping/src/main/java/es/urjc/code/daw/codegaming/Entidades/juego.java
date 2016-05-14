83c9ea4fa181dddbb941baab2fc515ac68e39e3f
valli22 <davidvacasmiguel@hotmail.com>
2016-05-14 14:17:06 +0200

    igual que antes (no se ha subido)

--- PracticaDAW_Spring/PracticaDAW_Sping/src/main/java/es/urjc/code/daw/codegaming/Entidades/juego.java
+++ PracticaDAW_Spring/PracticaDAW_Sping/src/main/java/es/urjc/code/daw/codegaming/Entidades/juego.java
@@ -1,3 +1,9 @@
+package es.urjc.code.daw.codegaming.Entidades;
+
+import java.util.List;
+
+import javax.persistence.*;
+
 @Entity
 public class Juego {
     @Id
@@ -32,8 +38,82 @@ public class Juego {
         
     }
     
-    public addRecomendacion(Recomendacion recom){
+    public void addRecomendacion(Recomendacion recom){
         this.recomendacion.add(recom);
     }
+
+	public String getImagen() {
+		return imagen;
+	}
+
+	public void setImagen(String imagen) {
+		this.imagen = imagen;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+
+	public float getValoracion() {
+		return valoracion;
+	}
+
+	public void setValoracion(float valoracion) {
+		this.valoracion = valoracion;
+	}
+
+	public List<String> getTags() {
+		return tags;
+	}
+
+	public void setTags(List<String> tags) {
+		this.tags = tags;
+	}
+
+	public List<String> getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(List<String> plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public List<Recomendacion> getRecomendacion() {
+		return recomendacion;
+	}
+
+	public void setRecomendacion(List<Recomendacion> recomendacion) {
+		this.recomendacion = recomendacion;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+    
+    
     
 }
